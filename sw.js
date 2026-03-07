012944028f2f3e913e7f403587b55b61995670a2
byCMMK <keepyourpassion@gmail.com>
2026-03-07 13:02:16 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,16 @@
 const CACHE = 'cmmk-v1';
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll([
-      './index.html',
-      './manifest.json',
-      './icon-192.png',
-      './icon-512.png'
-    ]))
-  );
   self.skipWaiting();
 });
 
-self.addEventListener('activate', e => { self.clients.claim(); });
+self.addEventListener('activate', e => {
+  // 모든 캐시 삭제
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
+  self.clients.claim();
+});
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
-      const rc = res.clone();
-      caches.open(CACHE).then(c => c.put(e.request, rc));
-      return res;
-    }))
-  );
+  // 캐시 없이 항상 네트워크에서 직접 받아옴
+  e.respondWith(fetch(e.request));
 });
