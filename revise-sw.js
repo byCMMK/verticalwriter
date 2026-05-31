de5cfd68abe71c1a0cfd07b7e825623bb552c738
byCMMK <keepyourpassion@gmail.com>
2026-05-31 22:51:56 +0900

    Update revise-sw.js

--- revise-sw.js
+++ revise-sw.js
@@ -1,24 +1,14 @@
 const CACHE = 'revise-v1';
-const ASSETS = ['/revise.html'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS))
-  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
   self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+  e.respondWith(fetch(e.request));
 });
